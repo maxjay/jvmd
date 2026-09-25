dc1ff237149410450c28bac460e2d8f272f1ddaf
Max <35901228+maxjay@users.noreply.github.com>
2026-09-25 03:32:35 +0100

    test: allow named public fixture source files

--- jvmd-tests/src/test/java/dev/jvmd/tests/IndexFixtures.java
+++ jvmd-tests/src/test/java/dev/jvmd/tests/IndexFixtures.java
@@ -6,7 +6,10 @@ import javax.tools.ToolProvider;
 /** Implements 12.4: compiled jar/source pairs that exercise generic and multi-release metadata. */
 final class IndexFixtures {
     static Path jar(Path directory,String name,String source,boolean parameters)throws Exception{
-        Files.createDirectories(directory);Path input=directory.resolve("Sample.java"),classes=Files.createDirectories(directory.resolve(name+"-classes"));Files.writeString(input,source);
+        return jar(directory,name,"Sample.java",source,parameters);
+    }
+    static Path jar(Path directory,String name,String fileName,String source,boolean parameters)throws Exception{
+        Files.createDirectories(directory);Path input=directory.resolve(fileName),classes=Files.createDirectories(directory.resolve(name+"-classes"));Files.writeString(input,source);
         var args=new ArrayList<>(List.of("-g","-d",classes.toString()));if(parameters)args.add("-parameters");args.add(input.toString());
         if(ToolProvider.getSystemJavaCompiler().run(null,null,null,args.toArray(String[]::new))!=0)throw new AssertionError("fixture compiler failed");
         Path jar=directory.resolve(name+".jar");try(var output=new JarOutputStream(Files.newOutputStream(jar));var files=Files.walk(classes)){for(var p:files.filter(Files::isRegularFile).toList()){output.putNextEntry(new JarEntry(classes.relativize(p).toString()));Files.copy(p,output);output.closeEntry();}}
